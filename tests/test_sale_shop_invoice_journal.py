d3236956955576bce5b8e06643e2ba002aae2a1e
resteve <resteve@zikzakmedia.com>
2015-12-01 11:22:47 +0100

    Tests

--- tests/test_sale_shop_invoice_journal.py
+++ tests/test_sale_shop_invoice_journal.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part of sale_shop_invoice_journal module for Tryton.
+# This file is part of the sale_shop_invoice_journal module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import trytond.tests.test_tryton
 import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleShopInvoiceJournalTestCase(unittest.TestCase):
+class SaleShopInvoiceJournalTestCase(ModuleTestCase):
     'Test Sale Shop Invoice Journal module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_shop_invoice_journal')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_shop_invoice_journal')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_shop_invoice_journal'
 
 
 def suite():
